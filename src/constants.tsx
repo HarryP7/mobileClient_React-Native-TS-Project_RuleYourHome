d847a76504b201eb7d9e2beb154b8b505d531c59
HarryP7 <grishkamartov@gmail.com>
2020-02-17 21:48:44 +0300

    изменение основного цвета приложения

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,18 +1,28 @@
 import * as React from 'react';
-import {Dimensions, Image, View} from 'react-native'
-import {globalStyles} from './components'
-const {im} = globalStyles
+import { Dimensions, Image, View, StyleSheet } from 'react-native'
 
 const win = Dimensions.get('window')
 export const h = win.height;
 export const w = win.width;
-export const ColorApp = '#92582D'
+export const appColor = '#009999'//'#92582D'
+export const disColor = '#999'
 export const BackgroundColor = 'lightsteelblue'
 export const NoFoto = "https://i.ibb.co/wNtDpNt/NoFoto.png"
 export const NoAvatar = "https://docplus.kg/img/noavatar.png"
-export const serverUrl = 'http://192.168.43.80:5000/api/' 
+export const serverUrl = 'http://192.168.43.80:5000/api/'
 export const BackgroundImage = require('../image/brick_texture1.jpg')
-export const Background = <View style={[im,{backgroundColor:BackgroundColor}]}></View> //<Image source={BackgroundImage} style={im}></Image>
 export const months = [
-    'января','февраля','марта','апреля','мая','июня','июля','августа',
-    'сентября','октября','ноября','декабря']
+    'января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа',
+    'сентября', 'октября', 'ноября', 'декабря']
+
+const styles = StyleSheet.create({
+    im: {
+        position: 'absolute',
+        height: h,
+        width: w
+    }
+});
+
+const { im } = styles
+export const Background = <View style={[im, { backgroundColor: BackgroundColor }]}></View> //<Image source={BackgroundImage} style={im}></Image>
+
